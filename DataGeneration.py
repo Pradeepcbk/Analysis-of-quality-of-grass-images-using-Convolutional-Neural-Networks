7c0ff8b82d5f82bfe058b804a9df23595d489280
Pradeep <34934343+Pradeepcbk@users.noreply.github.com>
2019-02-02 13:50:50 +0100

    Update DataGeneration.py

--- DataGeneration.py
+++ DataGeneration.py
@@ -14,19 +14,19 @@ def dataGeneration():
                                    horizontal_flip = True,
                                    rotation_range = 15)
     test_datagen = ImageDataGenerator(rescale = 1./255)    
-    training_set = train_datagen.flow_from_directory('test', 
+    training_set = train_datagen.flow_from_directory('data/training_data', 
                                                      target_size=(128,128),
                                                      color_mode='rgb',
                                                      batch_size = 2,
                                                      class_mode = 'binary'
                                                      )    
-    valid_set = test_datagen.flow_from_directory('Validation',
+    valid_set = test_datagen.flow_from_directory('data/validation_data',
                                                   target_size = (128,128),
                                                   color_mode = 'rgb',
                                                   batch_size = 2,
                                                   class_mode = 'binary'
                                                   )    
-    test_set = test_datagen.flow_from_directory('Dataset/test_data',
+    test_set = test_datagen.flow_from_directory('data/test_data',
                                                 target_size = (128,128),
                                                 class_mode = 'binary',
                                                 shuffle = False,
